ab7bd31c9b67defd55ddb3e5f956bb4bb444064f
Andrew Plotkin <erkyrath@eblong.com>
2011-04-13 00:31:12 -0400

    Changed the load-file interface to a table view. (I hope I know how this will work for save views...)

--- Classes/GlkFileSelectViewController.h
+++ Classes/GlkFileSelectViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 
-@interface GlkFileSelectViewController : UIViewController {
+@interface GlkFileSelectViewController : UITableViewController {
 }
 
 - (IBAction) buttonCancel:(id)sender;
--- Classes/GlkFileSelectViewController.m
+++ Classes/GlkFileSelectViewController.m
@@ -2,7 +2,7 @@
 //  GlkFileSelectViewController.m
 //  IosGlk
 //
-//  Created by Andrew Plotkin on 4/11/11.
+//  Created by Andrew Plotkin on 4/12/11.
 //  Copyright 2011 Andrew Plotkin. All rights reserved.
 //
 
@@ -10,7 +10,6 @@
 
 
 @implementation GlkFileSelectViewController
-
 
 - (void) viewDidLoad {
 	[super viewDidLoad];
@@ -35,10 +34,91 @@
 	NSLog(@"buttonCancel");
 }
 
-- (void) setEditing:(BOOL)editing animated:(BOOL)animated {
-	[super setEditing:editing animated:animated];
-	NSLog(@"setEditing now %d", editing);
+- (BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+	return YES;
 }
+
+
+// Table view data source
+
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView {
+	return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	return 6; //###?
+}
+
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+	static NSString *CellIdentifier = @"Cell";
+
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+	if (cell == nil) {
+		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+	}
+
+	// Configure the cell...
+
+	return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+	// Return NO if you do not want the specified item to be editable.
+	return YES;
+}
+*/
+
+
+/*
+// Override to support editing the table view.
+- (void) tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+
+	if (editingStyle == UITableViewCellEditingStyleDelete) {
+		// Delete the row from the data source.
+		[tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+	}
+	else if (editingStyle == UITableViewCellEditingStyleInsert) {
+		// Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+	}
+}
+*/
+
+
+/*
+// Override to support rearranging the table view.
+- (void) tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL) tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+	// Return NO if you do not want the item to be re-orderable.
+	return YES;
+}
+*/
+
+
+// Table view delegate
+
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	// Navigation logic may go here. Create and push another view controller.
+	/*
+	<#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+	// ...
+	// Pass the selected object to the new view controller.
+	[self.navigationController pushViewController:detailViewController animated:YES];
+	[detailViewController release];
+	*/
+}
+
 
 - (void) didReceiveMemoryWarning {
 	// Releases the view if it doesn't have a superview.
@@ -47,5 +127,5 @@
 	// Release any cached data, images, etc. that aren't in use.
 }
 
-
 @end
+
