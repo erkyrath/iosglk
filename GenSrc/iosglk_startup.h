e2de288f50fdca6622bb8b1f92a342aa407c7d6a
Andrew Plotkin <erkyrath@eblong.com>
2012-04-02 22:13:20 -0400

    Yeah, that startup change was a bad idea.

--- GenSrc/iosglk_startup.h
+++ GenSrc/iosglk_startup.h
@@ -1,8 +1,2 @@
 
-#ifdef __OBJC__
-/* The following function can't be called from C, only from ObjC. */
-@class NSString;
-extern void iosglk_set_game_path(NSString *path);
-#endif
-
 extern void iosglk_startup_code(void);
